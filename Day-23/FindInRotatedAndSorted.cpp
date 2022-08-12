419d39c5cb11d8b764e746e4ccfbfc3270cd41bb
Bikash Chauhan <chauhanbikash789@gmail.com>
2022-08-13 00:26:46 +0530

    Improved the algorithm for finding pivot Index

--- Day-23/FindInRotatedAndSorted.cpp
+++ Day-23/FindInRotatedAndSorted.cpp
@@ -3,11 +3,26 @@
 
 using namespace std;
 
+//finding the pivot element index
 int findPivotIndex(vector<int> &arr)
 {
-    
+    int start=0, end=arr.size()-1, mid;
+    while(start<=end)
+    {
+        mid = start+(end-start)/2;
+        if(arr[mid] >= arr[0])
+        {
+            start = mid+1;
+        }
+        else if(arr[mid] < arr[0])
+        {
+            end = mid-1;
+        }
+    }
+    return end;
 }
 
+//binary search algorithm
 int BS(vector<int> &arr, int start, int end, int target)
 {
     if (start <= end)
@@ -28,19 +43,27 @@ int BS(vector<int> &arr, int start, int end, int target)
     return -1;
 }
 
+//Search algorithm
 int search(vector<int> &nums, int target)
 {
-    // find the pivot (peak index)
-    int peakIndex = findPivotIndex(nums);
+    // find the pivot
+    int pivotIndex = findPivotIndex(nums);
 
-    if (peakIndex == -1)
-        return BS(nums, 0, nums.size() - 1, target);
-    // search in left peak of peak
-    int ans = BS(nums, 0, peakIndex, target);
+    if(target >= nums[0] && target <= nums[pivotIndex]) return BS(nums, 0, pivotIndex, target);
 
-    // if not found in left, search in right part of peak
-    if (ans == -1)
-        ans = BS(nums, peakIndex + 1, nums.size() - 1, target);
+    return BS(nums, pivotIndex + 1, nums.size() - 1, target);
+}
 
-    return ans;
+int main()
+{
+    int n, temp, target;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0; i<n; i++)
+    {
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    cin>>target;
+    cout<<search(arr, target)<<endl;
 }
