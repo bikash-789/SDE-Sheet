f0ac8f7ea4cd3bcf49ab4d48ee7bbd99e0b4030f
Bikash Chauhan <chauhanbikash789@gmail.com>
2022-08-14 15:54:09 +0530

    Updated the algorithm to find pivotIndex

--- Day-24/FindRotationCount.cpp
+++ Day-24/FindRotationCount.cpp
@@ -3,31 +3,44 @@
 
 using namespace std;
 
+int findPivotIndex(vector<int> &arr)
+{
+    int start = 0, end = arr.size() - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        // Checking if arr[mid] is pivot element or not
+        if (mid < end && arr[mid] > arr[mid + 1])
+        {
+            return mid;
+        }
+        if (mid > start && arr[mid] < arr[mid - 1])
+        {
+            return mid - 1;
+        }
+        if (arr[mid] <= arr[start])
+        {
+            end = mid - 1;
+        }
+        else
+            start = mid + 1;
+    }
+    return -1;
+}
 int findRotationCount(vector<int> &arr)
 {
-    //It's basically [pivotIndex + 1] 
-    int start=0, end=arr.size()-1, mid;
-    while(start<=end)
-    {
-        mid = start + (end-start)/2;
-        if(arr[mid]>=arr[0])
-        {
-            start = mid+1;
-        }
-        else end = mid-1;
-    }
-    return end+1;
+    return findPivotIndex(arr)+1;
 }
-
 int main()
 {
-    int n,temp;
-    cin>>n;
+    int n, temp;
+    cin >> n;
     vector<int> arr;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>temp;
+        cin >> temp;
         arr.push_back(temp);
     }
-    cout<<findRotationCount(arr)<<endl;
+    cout << findRotationCount(arr) << endl;
 }
