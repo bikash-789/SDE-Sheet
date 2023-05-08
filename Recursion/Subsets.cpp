b42f8736bc31f69c685396a2e0549e10510e7d31
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-05-08 08:54:26 +0545

    Generate Subsets problem solved

---
+++ Recursion/Subsets.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void generateSubsets(vector<int> arr, vector<int> subsets, int index)
+{
+    // base case
+    if(index == arr.size()-1)
+    {
+        if(subsets.size()==0)
+        {
+            cout<<"[]"<<endl;
+        }
+        // print the subset array
+        for(int i=0; i<subsets.size(); i++)
+        {
+            cout<<subsets[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+
+    
+    // include the current element pointed by index
+    subsets.push_back(arr[index]);
+    // recursive call
+    generateSubsets(arr, subsets, index+1);
+
+    // do not include the current element pointed by index
+    subsets.pop_back();
+    // recursive call
+    generateSubsets(arr, subsets, index+1);
+}
+int main()
+{
+    int n, val;
+    vector<int> arr, subsets;
+    cin>>n;
+    while(n--)
+    {
+        cin>>val;
+        arr.push_back(val);
+    }
+    generateSubsets(arr, subsets, 0);
+}
