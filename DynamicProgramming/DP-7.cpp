eb161606d41508ca8e9767613e058da634ea845e
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-12-27 00:45:08 +0530

    Tabulated and Optimized Version

--- DynamicProgramming/DP-7.cpp
+++ DynamicProgramming/DP-7.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 // Problem link: https://www.codingninjas.com/studio/problems/total-unique-paths_1081470
 
-int uniquePaths(int m, int n) {
+// int uniquePaths(int m, int n) {
+// 	// base case
+// 	if(m==0 && n==0)
+// 	{
+// 		return 1;
+// 	}
+// 	if(m<0 || n<0)
+// 	{
+// 		return 0;
+// 	}
+// 	// recursive call
+// 	int left = uniquePaths(m, n-1);
+// 	int up = uniquePaths(m-1, n);
+
+// 	return left+up;
+// }
+
+// Memoized Version
+int uniquePaths(int m, int n, vector<vector<int>> &dp)
+{
 	// base case
 	if(m==0 && n==0)
 	{
@@ -13,12 +32,46 @@ int uniquePaths(int m, int n) {
 		return 0;
 	}
 	// recursive call
-	int left = uniquePaths(m, n-1);
-	int up = uniquePaths(m-1, n);
+	if(dp[m][n] != -1) return dp[m][n];
+	int left = uniquePaths(m, n-1, dp);
+	int up = uniquePaths(m-1, n, dp);
 
-	return left+up;
+	return dp[m][n] = left+up;
 }
 
+// Tabulated Version - DP
+int uniquePaths(int m, int n)
+{
+	vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+	dp[0][1] = 1;
+
+	for(int i=1; i<=m; i++)
+	{
+		for(int j=1; j<=n; j++)
+		{
+			dp[i][j] = dp[i][j-1] + dp[i-1][j];
+		}
+	}
+	return dp[m][n];
+}
+
+// Optimized version
+int uniquePathsO(int m, int n)
+{
+	vector<int> dp(n+1,0);
+	dp[1] = 1;
+
+	for(int i=1; i<=m; i++)
+	{
+		vector<int> temp(n+1,0);
+		for(int j=1; j<=n; j++)
+		{
+			temp[j] = temp[j-1] + dp[j];
+		}
+		dp = temp;
+	}
+	return dp[n];
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -27,6 +80,9 @@ int main()
 #endif
 	int m, n;
 	cin>>m>>n;
-	cout<<uniquePaths(m-1,n-1)<<endl;
+	vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+	cout<<uniquePaths(m-1,n-1, dp)<<endl;
+	cout<<uniquePaths(m, n)<<endl;
+	cout<<uniquePathsO(m, n)<<endl;
 }
 
