6acaf10d9533269c1056272c362f8d738844bf51
Bikash Chauhan <chauhanbikash789@gmail.com>
2024-01-14 00:51:28 +0530

    Sorting Revision - O(n2)

---
+++ QuickRevision/Sorting.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void bubbleSort(vector<int> &arr, int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<(n-i-1); j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				swap(arr[j], arr[j+1]);
+			}
+		}
+	}
+}
+
+void insertionSort(vector<int> &arr, int n)
+{
+	for(int i=1; i<n; i++)
+	{
+		int j=i;
+		while(j>0 && arr[j] < arr[j-1])
+		{
+			swap(arr[j], arr[j-1]);
+			j--;
+		}
+	}
+}
+
+void selectionSort(vector<int> &arr, int n)
+{
+	// go and find the min element in the array
+	int minIndex = 0;
+	for(int i=0; i<n; i++)
+	{
+		minIndex = i;
+		for(int j=i+1; j<n; j++)
+		{
+			if(arr[j] < arr[minIndex])
+			{
+				minIndex = j;
+			}
+		}
+		swap(arr[i], arr[minIndex]);
+	}
+}
+
+void print(vector<int> &arr, int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin>>n;
+	vector<int> arr(n, 0);
+	for(int i=0; i<n; i++)
+	{
+		cin>>arr[i];
+	}
+	// bubbleSort(arr, n);
+	// print(arr, n);
+	// insertionSort(arr, n);
+	// print(arr, n);
+	selectionSort(arr, n);
+	print(arr, n);
+}
